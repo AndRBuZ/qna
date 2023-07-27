0261b02b32b332d121ff09178a0d84295a90eaff
AndRBuZ <andrrrw56@gmail.com>
2023-07-27 19:18:04 +0500

    mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,7 +65,7 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
 
   config.action_mailer.default_url_options = { host: 'http://89.108.81.99/' }
-  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.delivery_method = :sendmail
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
     port: 587,
